025f4d28a4627461e2807743aacc303385ec787b
wilsonjosue <wturpoh@unsa.edu.pe>
2023-06-27 18:49:45 -0500

    Se implementa los balances derecha e izquierda con los metodos de rotacion a la derecha y a la izquierda

--- Avl.java
+++ Avl.java
@@ -2,106 +2,129 @@ package eda.teo.pkg04;
 import myExceptions.ExceptionNoFound;
 
 public class Avl<E extends Comparable<E>> {
+
 	private NodeAvl<E> root;
     private boolean height;
-	
+
     public Avl() {
         this.root = null;
     }
-	
+
     public boolean isEmpty() {
-	    return this.root == null;
+        return this.root == null;
     }
-	
+
     public void insert(E x) throws ExceptionNoFound {
-	    this.root = insert(x, this.root);	
-	    this.height = false;
+        this.root = insert(x, this.root);
+        this.height = false;
     }
-	
-    private NodeAvl<E> insert(E x, NodeAvl<E> current) throws ExceptionNoFound{
-	    NodeAvl<E> res = current;
-	   if (current == null) {
-	       res = new NodeAvl<E>(x);
-	       this.height = true;
-	    }
-	    else {
-	        int resC = current.getData().compareTo(x);
-	        if (resC == 0)
-		        throw new ExceptionNoFound("Elemento ya se encuentra en el arbol");
-	        if (resC < 0) {
-		        res.setRight(insert(x, current.getRight()));
-	            if (this.height) {
-		            switch(res.getBf()) {
-		                case -1: res.setBf(0);
-			                this.height = false;
-			                break;
-			            case 0: res.setBf(1);
-			                break;
-			            case 1: //res.setBf(2);
-			                res = balanceToLeft(res);
-			                this.height = false;
-			                break;
-		            }
-                }	
-	        }
-	        else {
-		        res.setLeft(insert(x, current.getLeft()));
-		        /// completar la verificación de los factores de balance
-	        }
+
+    private NodeAvl<E> insert(E x, NodeAvl<E> current) throws ExceptionNoFound {
+        NodeAvl<E> res = current;
+        if (current == null) {
+            res = new NodeAvl<E>(x);
+            this.height = true;
+        } else {
+            int resC = current.getData().compareTo(x);
+            if (resC == 0)
+                throw new ExceptionNoFound("Elemento ya se encuentra en el arbol");
+            if (resC < 0) {
+                res.setRight(insert(x, current.getRight()));
+                if (this.height) {
+                    switch (res.getBf()) {
+                        case -1:
+                            res.setBf(0);
+                            this.height = false;
+                            break;
+                        case 0:
+                            res.setBf(1);
+                            break;
+                        case 1:
+                            res = balanceToLeft(res);
+                            this.height = false;
+                            break;
+                    }
+                }
+            } else {
+                res.setLeft(insert(x, current.getLeft()));
+                // completar la verificación de los factores de balance
+            }
         }
-	    return res;	
+        return res;
     }
 
-	private NodeAvl<E> balanceToLeft(NodeAvl<E> node){
-		
-	}
-	
-	private NodeAvl<E> rotateRSL(NodeAvl<E> node){
+    private NodeAvl<E> balanceToLeft(NodeAvl<E> node) {
+        NodeAvl<E> son = node.getRight();
+        if (son.getBf() == 1) {
+            node.setBf(0);
+            son.setBf(0);
+            node = rotateRSL(node);
+        } else if (son.getBf() == -1) {
+            NodeAvl<E> gSon = son.getLeft();
+            switch (gSon.getBf()) {
+                case -1:
+                    node.setBf(0);
+                    son.setBf(-1);
+                    break;
+                case 0:
+                    node.setBf(0);
+                    son.setBf(0);
+                    break;
+                case 1:
+                    node.setBf(1);
+                    son.setBf(0);
+                    break;
+            }
+            gSon.setBf(0);
+            node.setRight(rotateRSR(son));
+            node = rotateRSL(node);
+        }
+        return node;
+    }
 
-	}
+    private NodeAvl<E> balanceToRight(NodeAvl<E> node) {
+        NodeAvl<E> son = node.getLeft();
+       if (son.getBf() == -1) {
+            node.setBf(0);
+            son.setBf(0);
+            node = rotateRSR(node);
+        } 
+		else if (son.getBf() == 1) {
+            NodeAvl<E> gSon = son.getRight();
+            switch (gSon.getBf()) {
+                case 1:
+                    node.setBf(0);
+                    son.setBf(1);
+                    break;
+                case 0:
+                    node.setBf(0);
+                    son.setBf(0);
+                    break;
+                case -1:
+                    node.setBf(-1);
+                    son.setBf(0);
+                    break;
+           }
+           gSon.setBf(0);
+           node.setLeft(rotateRSL(son));
+            node = rotateRSR(node);
+        }
+        return node;
+    }
 	
-	private NodeAvl<E> rotateRSR(NodeAvl<E> node){
-		
-	}
+	private NodeAvl<E> rotateRSL(NodeAvl<E> node) {
+        NodeAvl<E> son = node.getRight();
+        node.setRight(son.getLeft());
+        son.setLeft(node);
+        node = son;
+        return node;
+    }
 
-	public E getRoot() {
-		return this.root.getData();
-	}
-	
-	
-	public E search(E x) throws ExceptionNoFound {
-		
-	}
-	
-	private NodeAvl<E> search(E x, NodeAvl<E> current) throws ExceptionNoFound{
-		
-		
-	}
-	
-	//La eliminación debe ser modificada a efecto de verificar el balanceo del árbol.
-	//Esta eliminación es del BST
-
-	public void remove(E x) throws ExceptionNoFound {
-		this.root = remove(x, this.root);	
-	}
-	
-	private NodeAvl<E> remove(E x, NodeAvl<E> current) throws ExceptionNoFound{
-		
-			
-	}
-	
-	private boolean isLeaf(NodeAvl<E> current) {
-		
-	}
-	
-	public void inOrden() {
-		
-	}
-	
-	
-	
-	private void inOrden(NodeAvl<E> current) {
-
-	}
-
+    private NodeAvl<E> rotateRSR(NodeAvl<E> node) {
+        NodeAvl<E> son = node.getLeft();
+        node.setLeft(son.getRight());
+        son.setRight(node);
+        node = son;
+        return node;
+    }
 }
