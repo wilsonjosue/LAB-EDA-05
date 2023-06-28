99c6d1a5a43f4de4e691c33a311f650f200bf079
wilsonjosue <wturpoh@unsa.edu.pe>
2023-06-27 19:09:49 -0500

    Se ingresa un ejemplo para probar como va nuestro codigo en TestBst.java

--- TestBst.java
+++ TestBst.java
@@ -5,6 +5,19 @@ public class TestBst {
 
 	public static void main(String[] args) throws ExceptionNoFound {
 		Avl<Integer> b = new Avl<Integer>();
+
+	    b.insert(20);
+     	b.inOrden();
+	    b.insert(30);
+	    b.inOrden();
+	    System.out.println("root: " + b.getRoot());
+	    b.insert(40);
+        b.inOrden();
+      	System.out.println("root: " + b.getRoot());
+	    b.insert(50);
+	    b.inOrden();
+	    b.insert(60);
+	    b.inOrden();
 	}
 
 }
